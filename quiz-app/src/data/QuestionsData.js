92d324c542c6435d0386856cf5aa9b7abd3e61b0
AUU_S44112266\RO <chawut.sa@gmail.com>
2024-01-16 06:52:50 +0700

    src/data/QuestionsData

---
+++ quiz-app/src/data/QuestionsData.js
@@ -0,0 +1,44 @@
+const QuestionsData = [
+    {
+        question: "1 + 1 ?",
+        A: "1",
+        B: "2",
+        C: "3",
+        D: "4",
+        answer: "2"
+    },
+    {
+        question: "1 + 2 ?",
+        A: "1",
+        B: "2",
+        C: "3",
+        D: "4",
+        answer: "C"
+    },
+    {
+        question: "1 + 3 ?",
+        A: "1",
+        B: "2",
+        C: "3",
+        D: "4",
+        answer: "D"
+    },
+    {
+        question: "2 - 1 ?",
+        A: "1",
+        B: "2",
+        C: "3",
+        D: "4",
+        answer: "A"
+    },
+    {
+        question: "2 + 2 ?",
+        A: "1",
+        B: "2",
+        C: "3",
+        D: "4",
+        answer: "D"
+    },
+]
+
+export default QuestionsData
